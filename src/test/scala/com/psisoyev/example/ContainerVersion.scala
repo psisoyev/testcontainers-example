af08540b144c66005bb2c79e629c901bfb342a34
Pavels Sisojevs <p.sisoyev@gmail.com>
2020-05-03 14:06:08 +0200

    implement different versions spec

--- src/test/scala/com/psisoyev/example/ContainerVersion.scala
+++ src/test/scala/com/psisoyev/example/ContainerVersion.scala
@@ -2,5 +2,7 @@ package com.psisoyev.example
 
 object ContainerVersion {
   val ApachePulsar: String = "2.2.0"
+  val ApachePulsarNew: String = "2.5.0"
   val MockServer: String = "5.5.4"
+  val Nginx: String = "latest"
 }
---
+++ src/test/scala/com/psisoyev/example/DifferentVersionsSpec.scala
@@ -0,0 +1,40 @@
+package com.psisoyev.example
+
+import com.dimafeng.testcontainers.lifecycle.and
+import com.dimafeng.testcontainers.{PulsarContainer, TestContainersForAll}
+import com.sksamuel.pulsar4s.{PulsarClient, Topic}
+import munit.FunSuite
+
+class DifferentVersionsSpec extends FunSuite with TestContainersForAll {
+  type Containers = PulsarContainer and PulsarContainer
+
+  override def startContainers(): and[PulsarContainer, PulsarContainer] = {
+    val pulsarContainer = PulsarContainer.Def(ContainerVersion.ApachePulsar).start()
+    val pulsarContainerNew = PulsarContainer.Def(ContainerVersion.ApachePulsarNew).start()
+
+    pulsarContainer and pulsarContainerNew
+  }
+
+  val message = "hello"
+
+  test("Send and receive a message") {
+    withContainers { case pulsarContainer and pulsarContainerNew =>
+      List(pulsarContainer, pulsarContainerNew).map { container =>
+        val result = scenario(container.pulsarBrokerUrl())
+
+        assert(result == Right(message))
+      }
+    }
+  }
+
+  def scenario(brokerUrl: String): Either[Throwable, String] = {
+    val client: PulsarClient = PulsarClient(brokerUrl)
+    val topic = Topic("non-persistent://sample/standalone/test/123")
+
+    val service = new MyPulsarService(client, topic)
+
+    service.send(message)
+
+    service.receive
+  }
+}
